d1a0ac7579a1ae907257a10a8ef48a60263424e1
Thesiddemigod <153060675+Thesiddemigod@users.noreply.github.com>
2024-10-09 00:31:11 +0530

    Update Searchbar.tsx

--- components/shared/Searchbar.tsx
+++ components/shared/Searchbar.tsx
@@ -40,8 +40,7 @@ function Searchbar({ routeType }: Props) {
         id='text'
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-        placeholder={`${routeType !== "/search" ? "Search communities" : "Search creators"
-          }`}
+        placeholder={`${routeType === "search" ? "Search creators" : "Search communities"}`}
         className='no-focus searchbar_input'
       />
     </div>
